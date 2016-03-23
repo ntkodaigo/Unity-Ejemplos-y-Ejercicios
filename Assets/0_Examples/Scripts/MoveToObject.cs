54650bf23497274a68e111bdcb57262821430926
Daigo <u2011115775@continental.edu.pe>
2016-03-23 18:18:38 -0500

    Ejemplos de movimientos sin usar fisica
    Ejemplos de movimientos sin usar fisica, solo usando el Transform,
    teniendo en cuenta las perspectivas Global y Local y el uso de Vectores
    y sus operaciones.

---
+++ Assets/0_Examples/Scripts/MoveToObject.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class MoveToObject : MonoBehaviour
+{
+    /// <summary>
+    /// Lista de objetos a seguir
+    /// </summary>
+    public Transform[] objectToFollowList;
+    /// <summary>
+    /// Velocidad de movimiento por segundo
+    /// </summary>
+    public float moveSpeed = 3f;
+    /// <summary>
+    /// Distancia minima para detenerse
+    /// </summary>
+    public float threshold = 0.5f;
+    /// <summary>
+    /// Bool que identifica que el objeto esta o debe moverse
+    /// </summary>
+    public bool isMoving;
+    /// <summary>
+    /// Bool para ver si esta caminando o corriendo
+    /// </summary>
+    public bool isWalking;
+
+    private Transform m_MyTransform;
+    private Animator m_MyAnimator;
+    /// <summary>
+    /// Indice del actual objeto a seguir
+    /// </summary>
+    private int m_CurrentObjectToFollowIndex;
+
+	void Start ()
+    {
+        m_MyTransform = GetComponent<Transform>();
+        m_MyAnimator = GetComponent<Animator>();
+
+        if (objectToFollowList.Length == 0)
+        {
+            Debug.LogError("I need least one objectToFollow in my inspector.");
+        }
+	}
+	
+	void Update ()
+    {
+        // Verificamos que tengamos objetos en la lista y podemos movernos
+        if (objectToFollowList.Length > 0 && isMoving)
+        {
+            // Calculamos el Vector3 direccion con modulo 1 (normalizado) hacia el objetivo, visto desde myTransform
+            var directionToObject = (objectToFollowList[m_CurrentObjectToFollowIndex].position - m_MyTransform.position).normalized;
+            // Hacemos que el Vector3 blue z local sea el Vector3 direction pero con el eje 'y' 0 para que mire al objetivo ignorando su posicion em 'y'
+            m_MyTransform.forward = new Vector3(directionToObject.x, 0, directionToObject.z);
+
+            // Calculamos la distancia entre el objetivo y myTransform, si esa distancia aun no es menor que el threshold (distancia minima para detenernos), tenemos que seguir acercandonos al objetivo
+            if (Vector3.Distance(objectToFollowList[m_CurrentObjectToFollowIndex].position, m_MyTransform.position) > threshold)
+            {
+                // Movemos myTransform con Translate (perpectiva local), enviandole el Vector3 forward global como direccion, ya que ya tenemos el forward de myTransform apuntando hacia el objetivo
+                // Ademas lo multiplicamos por moveSpeed (velocidad de movimiento) y Time.deltaTime para que el movimiento sea a metros/segundo, la velocidad es la mitad si esta caminando
+                m_MyTransform.Translate(Vector3.forward * Time.deltaTime * (isWalking ? (moveSpeed * 0.5f) : moveSpeed));
+
+                // Ponemos el valor del parametro 'MoveSpeed' del Animator a 0.5f o 1f depende de si esta caminando o corriendo (isWalking == false)
+                m_MyAnimator.SetFloat("MoveSpeed", isWalking ? 0.5f : 1f);
+            }
+            else
+            {
+                // Si ya estamos muy cerca y podemos detenernos, ponemos el parametro del Animator a 0f para actualizar la animacion a Idle
+                m_MyAnimator.SetFloat("MoveSpeed", 0f);
+
+                // Aumentamos el indice para seguir al siguiente objeto
+                m_CurrentObjectToFollowIndex++;
+
+                // TO DO: Verificar el tamano de la lista y reiniciar a el indice actual:m_CurrentObjectToFollowIndex a 0
+            }
+        }
+        else
+        {
+            // Si ya estamos muy cerca y podemos detenernos, ponemos el parametro del Animator a 0f para actualizar la animacion a Idle
+            m_MyAnimator.SetFloat("MoveSpeed", 0f);
+        }
+	}
+}
---
+++ Assets/0_Examples/Scripts/ThirdPersonCharacter_Test.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCharacter_Test : MonoBehaviour
+{
+    private Transform m_MyTransform;
+    private Vector3 m_FromPivotToMouse;
+    private Camera m_MainCam;
+
+    void Start()
+    {
+        m_MyTransform = GetComponent<Transform>();
+        m_MainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        // Obtenemos el eje z del Vector3 calculado de myTransform (en el Mundo) a perpectiva de la Pantalla, como referencia para la posicion del mouse
+        var distanceToScreen = m_MainCam.WorldToScreenPoint(m_MyTransform.position).z;
+        // Calculamos la posicion del mouse de la pantalla, a perspectiva del mundo, usando el z en la pantalla de myTransfom
+        var mouseInWorld = m_MainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
+
+        // Calculamos el Vector3 direccion con modulo 1 (normalizado) hacia el mouse, visto desde myTranform
+        var directionToMouse = (mouseInWorld - m_MyTransform.position).normalized;
+        // Multiplicamos cada eje del Vector3 direccion hacia el mouse, con en nuevo Vector3 (1, 0, 1) para anular su eje 'y'
+        directionToMouse = Vector3.Scale(directionToMouse, new Vector3(1,0,1));
+        // Hacemos que el Vector3 blue z local sea el Vector3 direction pero con el eje 'y' 0 para que mire al objetivo ignorando su posicion en 'y'
+        m_MyTransform.forward = directionToMouse;
+
+        // Obtenemos los ejes Horizontal y Vertical y creamos un nuevo vector con modulo 1 (normalizado) como Vector3 de direccion
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        var dir = new Vector3(h, 0, v).normalized;
+
+        // Transladamos myTransform, enviandolo la direccion creado con los ejes Horizontal y Vectical, a una velocidad constante de 5 metros / segundo
+        // Translate, enviandole un solo parametro usa una perspectiva local para trasladar, por eso esta bien enviar el Vector3 dir (global) 
+        m_MyTransform.Translate(dir * Time.deltaTime * 5f);
+    }
+
+    
+    void OnMouseDown()
+    {
+        // Obtenemos el eje z del Vector3 calculado de myTransform (en el Mundo) a perpectiva de la Pantalla, como referencia para la posicion del mouse
+        var distanceToScreen = m_MainCam.WorldToScreenPoint(m_MyTransform.position).z;
+
+        // Obtenemos el Vector3 desde el Pivot (origen de la posicion) hacia el punto de toque del mouse y lo guardamos
+        m_FromPivotToMouse = m_MainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen)) - m_MyTransform.position;
+    }
+
+    void OnMouseDrag()
+    {
+        // Obtenemos el eje z del Vector3 calculado de myTransform (en el Mundo) a perpectiva de la Pantalla, como referencia para la posicion del mouse
+        var distanceToScreen = m_MainCam.WorldToScreenPoint(m_MyTransform.position).z;
+        // Obtenemos el Vector3 calculado desde la posicion del mouse con referencia al eje z de myTransform en perspectiva en pantalla, restado por Vector3 calculado anteriormente para obtener la posicion deseada
+        // fromPivotToMouse = mouseInWorld - myTransform.position -> myTransform.position = mouseInWorld - fromPivotToMouse
+        var mouseInWorld = m_MainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen)) - m_FromPivotToMouse;
+
+        m_MyTransform.position = mouseInWorld;
+    }
+}
